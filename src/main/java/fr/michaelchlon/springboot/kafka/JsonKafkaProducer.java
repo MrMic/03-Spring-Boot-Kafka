4b527a4bd8868fb0cd86ad49a5980e874fe89772
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-05-29 13:23:09 +0200

    feat(Spring Boot Kafka Producer + Consumer): 04/Create Kafka Producer to produce JSON Message
    feat: add JSON Kafka producer and update gitignore
    
    - Create JsonKafkaProducer to handle user messages
    - Integrate KafkaTemplate for message sending
    - Update .gitignore to include profiling files (*.profraw)

---
+++ src/main/java/fr/michaelchlon/springboot/kafka/JsonKafkaProducer.java
@@ -0,0 +1,34 @@
+package fr.michaelchlon.springboot.kafka;
+
+import fr.michaelchlon.springboot.payload.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JsonKafkaProducer {
+  private static final Logger LOGGER = LoggerFactory.getLogger(JsonKafkaProducer.class);
+
+  private KafkaTemplate<String, User> kafkaTemplate;
+
+  // * INFO: ╾╼ CONSTRUCTOR ╾───────────────────────────────────────────────────╼
+  @Autowired
+  public JsonKafkaProducer(KafkaTemplate<String, User> kafkaTemplate) {
+    this.kafkaTemplate = kafkaTemplate;
+  }
+
+  public void sendMessage(User user) {
+    LOGGER.info(String.format("#### -> USER Message sent: %s", user.toString()));
+
+    Message<User> message =
+        MessageBuilder.withPayload(user).setHeader(KafkaHeaders.TOPIC, "java_guides").build();
+
+    kafkaTemplate.send(message);
+    LOGGER.info(String.format("#### -> Message sent: %s", message));
+  }
+}
